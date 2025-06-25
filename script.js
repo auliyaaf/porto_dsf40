cc29f26f96be131468799bec7b34a8067d29736e
auliyaaf <rauliyafaizah@gmail.com>
2025-06-26 02:14:40 +0700

    init

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-    <script>
-      const lightbox = document.getElementById("lightbox");
-      const lightboxImg = lightbox.querySelector("img");
-
-      document.querySelectorAll(".project-item img").forEach((img) => {
-        img.addEventListener("click", () => {
-          lightboxImg.src = img.src;
-          lightbox.style.display = "flex";
-        });
-      });
-
-      lightbox.addEventListener("click", () => {
-        lightbox.style.display = "none";
-      });
-    </script>
+<script>
+  const lightbox = document.getElementById("lightbox"); const lightboxImg =
+  lightbox.querySelector("img"); document.querySelectorAll(".project-item
+  img").forEach((img) ={" "}
+  {img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = "flex";
+  })}
+  ); lightbox.addEventListener("click", () = {(lightbox.style.display = "none")}
+  );\
+</script>;
